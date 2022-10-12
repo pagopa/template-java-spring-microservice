8259cbc337b657294765ca5a758c35411c087e51
Jacopo <jacopo.carlini@gmail.com>
2022-10-12 13:06:33 +0200

    fix

--- src/main/java/it/gov/pagopa/microservice/controller/HomeController.java
+++ src/main/java/it/gov/pagopa/microservice/controller/HomeController.java
@@ -1,25 +1,10 @@
 package it.gov.pagopa.microservice.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import it.gov.pagopa.microservice.model.AppInfo;
-import it.gov.pagopa.microservice.model.ProblemJson;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.info.InfoEndpoint;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
-
-import java.util.Map;
 
 @RestController
 public class HomeController {
