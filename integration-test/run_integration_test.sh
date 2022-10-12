8b35d38e65c72702714acfb069030172d9418cc5
Jacopo <jacopo.carlini@gmail.com>
2022-10-12 13:06:23 +0200

    fix

--- integration-test/run_integration_test.sh
+++ integration-test/run_integration_test.sh
@@ -10,14 +10,15 @@ sh ./run_docker.sh "$1"
 printf 'Waiting for the service'
 attempt_counter=0
 max_attempts=50
-until $(curl --output /dev/null --silent --head --fail http://localhost:8080/info); do
+# shellcheck disable=SC2091
+until $(curl --output /dev/null --silent --head --fail http://localhost:8080/actuator/info); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached"
       exit 1
     fi
 
     printf '.'
-    attempt_counter=$(($attempt_counter+1))
+    attempt_counter=$((attempt_counter+1))
     sleep 10
 done
 echo 'Service Started'
--- src/main/java/it/gov/pagopa/microservice/controller/HomeController.java
+++ src/main/java/it/gov/pagopa/microservice/controller/HomeController.java
@@ -27,16 +27,6 @@ public class HomeController {
     @Value("${server.servlet.context-path}")
     String basePath;
 
-    @Value("${info.application.name}")
-    private String name;
-
-    @Value("${info.application.version}")
-    private String version;
-
-    @Value("${info.properties.environment}")
-    private String environment;
-
-
 
     /**
      * @return redirect to Swagger page documentation
@@ -50,24 +40,4 @@ public class HomeController {
         return new RedirectView(basePath + "swagger-ui.html");
     }
 
-
-    @Operation(summary = "health check", description = "Return OK if application is started", security = {@SecurityRequirement(name = "ApiKey")}, tags = {"Home"})
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = AppInfo.class))),
-            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class))),
-            @ApiResponse(responseCode = "401", description = "Unauthorized", content = @Content(schema = @Schema())),
-            @ApiResponse(responseCode = "403", description = "Forbidden", content = @Content(schema = @Schema())),
-            @ApiResponse(responseCode = "429", description = "Too many requests", content = @Content(schema = @Schema())),
-            @ApiResponse(responseCode = "500", description = "Service unavailable", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ProblemJson.class)))})
-    @GetMapping(value = "/info")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<AppInfo> healthCheck() {
-        // Used just for health checking
-        AppInfo info = AppInfo.builder()
-                .name(name)
-                .version(version)
-                .environment(environment)
-                .build();
-        return ResponseEntity.status(HttpStatus.OK).body(info);
-    }
 }
