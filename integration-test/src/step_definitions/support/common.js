658a0f25ed8e4a795d7ca89e93a97a9262bae16c
Jacopo <jacopo.carlini@gmail.com>
2023-04-14 16:24:40 +0200

    header canary

--- integration-test/src/step_definitions/support/common.js
+++ integration-test/src/step_definitions/support/common.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 axios.defaults.headers.common['Ocp-Apim-Subscription-Key'] = process.env.subkey // for all requests
 if (process.env.canary) {
-  axios.defaults.headers.common['X-Pod-Version'] = 'canary' // for all requests
+  axios.defaults.headers.common['X-Canary'] = 'canary' // for all requests
 }
 
 function get(url) {
