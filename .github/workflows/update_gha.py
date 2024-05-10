a08d83084993635f6d56b1e83bf9f11395b4a036
Jacopo <jacopo.carlini@gmail.com>
2024-05-10 12:19:58 +0200

    update gha

--- .github/workflows/update_gha.py
+++ .github/workflows/update_gha.py
@@ -1,19 +1,16 @@
-import requests
 import json
+import requests
 
-
-url = 'https://github.com/pagopa/template-java-spring-microservice/tree/main/.github/workflows'
+url = 'https://api.github.com/repos/pagopa/template-java-spring-microservice/contents/.github/workflows'
 url_raw = 'https://raw.githubusercontent.com/pagopa/template-java-spring-microservice/main/'
 
 response = requests.get(url)
 
-for item in json.loads(response.text)["payload"]["tree"]["items"]:
+for item in json.loads(response.text):
     path = item["path"]
     name = item["name"]
     print(name)
-    response = requests.get(url_raw+path)
+    response = requests.get(url_raw + path)
     fo = open(name, "w")
     fo.write(response.text)
     fo.close()
-
-
