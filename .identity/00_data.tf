ab95a2157e0d8478c7d16251cced7ebfe1805915
Jacopo Carlini <jacopo.carlini@pagopa.it>
2024-11-21 11:56:22 +0100

    feat: new GHA and identity

--- .identity/00_data.tf
+++ .identity/00_data.tf
@@ -17,23 +17,25 @@ data "github_organization_teams" "all" {
   summary_only    = true
 }
 
-data "azurerm_user_assigned_identity" "identity_cd_01" {
-  name                = "${local.prefix}-${var.env_short}-${local.domain}-01-github-cd-identity"
-  resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
-}
-
 data "azurerm_key_vault" "key_vault" {
   name                = "pagopa-${var.env_short}-kv"
   resource_group_name = "pagopa-${var.env_short}-sec-rg"
 }
 
+
+data "azurerm_user_assigned_identity" "identity_cd_01"{
+  name = "${local.prefix}-${var.env_short}-${local.domain}-01-github-cd-identity"
+  resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
+}
+
+data "azurerm_user_assigned_identity" "identity_pr_01" {
+  name                = "${local.prefix}-${var.env_short}-${local.domain}-01-pr-github-cd-identity"
+  resource_group_name = "${local.prefix}-${var.env_short}-identity-rg"
+}
+
 data "azurerm_key_vault" "domain_key_vault" {
   name                = "pagopa-${var.env_short}-${local.domain}-kv"
   resource_group_name = "pagopa-${var.env_short}-${local.domain}-sec-rg"
-}
-
-data "azurerm_resource_group" "apim_resource_group" {
-  name = "${local.product}-api-rg"
 }
 
 data "azurerm_key_vault_secret" "key_vault_sonar" {
